e0058d60b89c5d22cf0fbf2f759489da7fc58013
Amyat103 <zhoudavid7312@gmail.com>
2024-06-20 13:15:31 -0400

    feat styled navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,6 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className='navbar navbar-expand-lg navbar-light bg-body-tertiary'>
-      <div className='container-fluid d-flex justify-content-between align-items-center'>
-        {/* Logo and Site Title */}
+      <div className='container-fluid custom-container d-flex justify-content-between align-items-center flex-nowrap'>
         <Link className='navbar-brand d-flex align-items-center' to='/'>
           <img
             src={logo}
@@ -16,20 +16,8 @@ function Navbar() {
           Plate Critique
         </Link>
 
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNavDropdown'
-          aria-controls='navbarNavDropdown'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-
-        <div className='justify-content-end' id='navbarNavDropdown'>
-          <ul className='navbar-nav'>
+        <div className='d-flex justify-content-end'>
+          <ul className='navbar-nav flex-row'>
             <li className='nav-item'>
               <Link className='nav-link active' aria-current='page' to='/'>
                 Home
