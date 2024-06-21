69213d804239e47ae440f37845f77b0546b1d836
Amyat103 <zhoudavid7312@gmail.com>
2024-06-21 01:04:32 -0400

    backend url change for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(
   cors({
-    origin: 'platecritique-backend.up.railway.appp',
+    origin: 'https://platecritique.up.railway.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
