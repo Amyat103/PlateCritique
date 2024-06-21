322c5c7ba24c48d4f6c429ff565a88b63f34040d
Amyat103 <zhoudavid7312@gmail.com>
2024-06-20 23:36:38 -0400

    added railway url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 // app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(
   cors({
-    origin: '*',
+    origin: 'platecritique.up.railway.app',
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
